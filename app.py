7f81302c663d74c625376d03174ea9d831c24e47
reddypradeep9 <30616258+reddypradeep9@users.noreply.github.com>
2023-07-18 18:08:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import sklearn.liner_model.ridge
 
 # Load the Random Forest CLassifier model
 filename = 'ridge.pkl'
